8917a12a1fc6e5eaedbe41cdccde8dc098d8a4cf
Khisrav <kh.khisrav2018@gmail.com>
2023-11-26 12:12:21 +0300

    minor changes

---
+++ src/components/PaymentStatusToast.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- 
+        SVG error
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-8 sm:h-10" fill="none"
+        viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg> -->
+    <div class="toast toast-bottom toast-center z-50 mb-3 sm:toast-end w-auto box-border">
+        <div id="Success" class="w-auto">
+            <button role="alert" @click="Close('Success')"
+            class="alert alert-success text-sm sm:text-lg bg-white text-green-600  flex">
+            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-8 sm:h-10" fill="none"
+                viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <p>Your purchase has been confirmed!</p>
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'PaymentStatusToast',
+    methods: {
+        Close(a) {
+            const Close = document.getElementById(a)
+            Close.style.display = "none"
+        }
+    }
+}
+</script>
+  
--- src/components/PaymentView.vue
+++ src/components/PaymentView.vue
@@ -1,34 +1,105 @@
 <template>
-    <div class="container mx-auto mt-12 w-full md:w-1/3">
-        <div class="rounded-2xl p-10 bg-white shadow-2xl shadow-slate-200">
-            <div class="">
-                <form action="" class="">
-                    <div class="payment-card bg-orange-600 rounded-xl pt-6 pb-6">
-                        <div class="grid grid-cols-1 p-2 px-4">
-                            <label for="" class="label">
-                                <span class="label-text text-orange-50 font-medium">Номер карты</span>
-                                <span class="label-text-alt text-orange-50 font-bold">MasterCard</span>
-                            </label>
-                            <input type="text" class="input w-full bg-orange-500 text-orange-50 font-medium">
-                        </div>
-                        <div class="grid grid-cols-3 p-2 px-4">
-                            <div>
-                                <label for="" class="label mr-4">
-                                    <span class="label-text text-orange-50 font-medium">ММ\ГГ</span>
+    <div class="mockup-phone">
+        <div class="camera"></div> 
+        <div class="display">
+            <div class="artboard artboard-demo phone-1 bg-white">
+                <PaymentStatusToast v-if="false"></PaymentStatusToast>
+                <div class="container mx-auto w-full">
+                    <div class="">
+                        <img src="@/assets/logo.png" alt="" class="w-1/3 mx-auto qiwi-logo">
+                    </div>
+                    <div class="rounded-2xl p-3">
+                        <div class="">
+                            <form @submit="processPayment()">
+                                <div class="payment-card bg-orange-600 rounded-xl pb-2">
+                                    <div class="grid grid-cols-1 p-2 px-4">
+                                        <label for="" class="label">
+                                            <span class="label-text text-orange-50 font-medium">Номер карты</span>
+                                            <span v-if="cardType" class="label-text-alt text-orange-50 font-bold">{{ cardType }}</span>
+                                        </label>
+                                        <input v-model="cardNumber" @input="detectCardType" type="text" class="input w-full bg-orange-500 text-orange-50 font-medium">
+                                    </div>
+                                    <div class="grid grid-cols-2 p-2 pt-0 px-4">
+                                        <div class="mr-2">
+                                            <label for="" class="label mr-4">
+                                                <span class="label-text text-orange-50 font-medium">ММ\ГГ</span>
+                                            </label>
+                                            <input v-model="cardExpire" type="text" class="input w-full bg-orange-500 text-orange-50 font-medium">
+                                        </div>
+                                        <div class="ml-2">
+                                            <label for="" class="label">
+                                                <span class="label-text text-orange-50 font-medium">CVC/CVV</span>
+                                            </label>
+                                            <input v-model="cardCVC" type="number" class="input w-full bg-orange-500 text-orange-50 font-medium">
+                                        </div>
+                                    </div>
+                                </div>
+                                <label for="" class="label mt-2">
+                                    <span class="label-text font-medium">Сумма</span>
+                                    <span class="label-text-alt font-bold">РУБ</span>
                                 </label>
-                                <input type="text" class="input w-full bg-orange-500 text-orange-50 font-medium mr-4">
-                            </div>
-                            <div>
-                                <label for="" class="label">
-                                    <span class="label-text text-orange-50 font-medium ml-4">CVC/CVV</span>
-                                </label>
-                                <input type="text" class="input w-full bg-orange-500 text-orange-50 font-medium ml-4">
-                            </div>
+                                <input v-model="amount" type="text" class="input w-full bg-slate-100 input-bordered text-orange-50 font-medium">
+                                <button type="submit" class="btn btn-lg rounded-full w-full block mt-4 mx-auto btn-neutral">Оплатить</button>
+                            </form>
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-lg w-1/2 block mt-9 mx-auto btn-neutral">Оплатить</button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+import PaymentStatusToast from './PaymentStatusToast.vue';
+
+export default {
+    data() {
+        return {
+            cardNumber: null,
+            cardType: "",
+            cardExpire: "",
+            cardCVC: "",
+            amount: null
+        };
+    },
+    methods: {
+        detectCardType() {
+            const visaPattern = /^4[0-9]{12}(?:[0-9]{3})?$/;
+            const mirPattern = /^2[0-9]{15}$/;
+            const mastercardPattern = /^5[1-5][0-9]{14}$/;
+            if (visaPattern.test(this.cardNumber)) {
+                this.cardType = "Visa";
+            }
+            else if (mirPattern.test(this.cardNumber)) {
+                this.cardType = "Mir";
+            }
+            else if (mastercardPattern.test(this.cardNumber)) {
+                this.cardType = "MasterCard";
+            }
+            else {
+                this.cardType = "";
+            }
+        },
+        processPayment() {
+            console.log(this.cardNumber);
+            console.log(this.amount);
+            console.log(this.cardExpire);
+            console.log(this.cardCVC);
+            return false;
+        }
+    },
+    components: { PaymentStatusToast }
+};
+</script>
+
+<style>
+.mockup-phone {
+    position: absolute;
+    top:50%;
+    left:50%;
+    transform: translate(-50%, -50%);
+}
+.qiwi-logo {
+    filter: invert(.8)
+}
+</style>
